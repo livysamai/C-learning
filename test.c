58eaff6b5ba4e117a111c43c27be48a4e75182fa
livysamai <76438323+livysamai@users.noreply.github.com>
2020-12-25 10:02:26 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//计算两个数的和
+//int main()
+
+//{
+	//int sum = 0;//C语言规定，变量要定义在当前代码快的最前面
+	//int num1 = 0;
+	//int num2 = 0;
+	//scanf_s("%d%d", &num1, &num2);//&取地址符号  //输入数据--使用输入函数scanf。此版本需要scanf_s
+
+	//sum = num1 + num2;
+	//printf("sum = %d\n",sum);
+
+	//return 0;
+//}
+
+
---
+++ text2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int sum;
+    int num1 = 0;
+    int num2 = 0;
+scanf_s("%d%d", &num1, &num2);
+sum = num1 + num2;
+printf("sum = %d\n", sum);
+return 0;
+}
